5e510c6f69f47af7b5f926a86ae62182c9cb9516
youmna_dwidar <youmnadwidar@hotmail.com>
2017-04-15 01:15:27 +0200

    8

--- scr/src/eg/edu/alexu/csd/datastructure/hangman/cs74/HangMan.java
+++ scr/src/eg/edu/alexu/csd/datastructure/hangman/cs74/HangMan.java
@@ -10,75 +10,75 @@ import java.lang.String;
  */
 public class HangMan implements IHangman {
 
-	private String[] dic_Words = null;
-	private String secretword = null;
-	private String shownword = null;
-	public int maxNumofWrongGueses = 0;
+  private String[] dic_Words = null;
+  private String secretword = null;
+  private String shownword = null;
+  public int maxNumofWrongGueses = 0;
 
-	public void setDictionary(String[] words) {
+  public void setDictionary(String[] words) {
 
-		dic_Words = words;
+    dic_Words = words;
 
-	}
+  }
 
-	public String selectRandomSecretWord() {
-		if (dic_Words == null || dic_Words.length == 0) {
-			return null;
-		} else {
-			int x = (int) (Math.random() * (dic_Words.length));
-			this.secretword = this.dic_Words[x];
-			this.shownword = this.dic_Words[x];
+  public String selectRandomSecretWord() {
+    if (dic_Words == null || dic_Words.length == 0) {
+      return null;
+    } else {
+      int x = (int) (Math.random() * (dic_Words.length));
+      this.secretword = this.dic_Words[x];
+      this.shownword = this.dic_Words[x];
 
-			char[] word;
-			word = new char[this.shownword.length()];
-			word = this.shownword.toCharArray();
-			for (int i = 0; i < word.length; i++) {
-				// word.setCharAt(i, '-');
-				word[i] = '-';
-			}
-			this.shownword = String.valueOf(word);
+      char[] word;
+      word = new char[this.shownword.length()];
+      word = this.shownword.toCharArray();
+      for (int i = 0; i < word.length; i++) {
+        // word.setCharAt(i, '-');
+        word[i] = '-';
+      }
+      this.shownword = String.valueOf(word);
 
-			return secretword;
+      return secretword;
 
-		}
-	}
+    }
+  }
 
-	public String guess(Character c) {
-		if (secretword == null) {
-			return null;
+  public String guess(Character c) {
+    if (secretword == null) {
+      return null;
 
-		}
+    }
 
-		else if (c == null) {
-			return shownword;
+    else if (c == null) {
+      return shownword;
 
-		} else {
+    } else {
 
-			char[] word = new char[shownword.length()];
-			word = shownword.toCharArray();
+      char[] word = new char[shownword.length()];
+      word = shownword.toCharArray();
 
-			int flag = 0;
-			c = Character.toLowerCase(c.charValue());
+      int flag = 0;
+      c = Character.toLowerCase(c.charValue());
 
-			for (int i = 0; i < word.length; i++) {
-				if (c == Character.toLowerCase(secretword.charAt(i))) {
+      for (int i = 0; i < word.length; i++) {
+        if (c == Character.toLowerCase(secretword.charAt(i))) {
 
-					flag = 1;
-					word[i] = secretword.charAt(i);
-				}
-			}
-			if (flag == 0)
-				maxNumofWrongGueses--;
-			if ((maxNumofWrongGueses <= 0))
-				return null;
+          flag = 1;
+          word[i] = secretword.charAt(i);
+        }
+      }
+      if (flag == 0)
+        maxNumofWrongGueses--;
+      if ((maxNumofWrongGueses <= 0))
+        return null;
 
-			shownword = String.valueOf(word);
-			return shownword;
-		}
-	}
+      shownword = String.valueOf(word);
+      return shownword;
+    }
+  }
 
-	public void setMaxWrongGuesses(Integer max) {
-		this.maxNumofWrongGueses = max;
-	}
+  public void setMaxWrongGuesses(Integer max) {
+    this.maxNumofWrongGueses = max;
+  }
 
 }
