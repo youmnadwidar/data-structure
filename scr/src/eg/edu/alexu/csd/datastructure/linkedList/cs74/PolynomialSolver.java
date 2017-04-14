9963836840831181135d46068350dcdd3b8085fe
youmna_dwidar <youmnadwidar@hotmail.com>
2017-04-14 03:35:57 +0200

    8

--- scr/src/eg/edu/alexu/csd/datastructure/linkedList/cs74/PolynomialSolver.java
+++ scr/src/eg/edu/alexu/csd/datastructure/linkedList/cs74/PolynomialSolver.java
@@ -123,7 +123,7 @@ public class PolynomialSolver implements IPolynomialSolver {
 		R=new DoublyLinkedList();
 		if ((poly1 == 'A' || poly1 == 'B') && (poly2 == 'A' || poly2 == 'B')) {
 			if (A == null || B== null)
-				throw new RuntimeException();
+				throw null;
 			addLinked(A, B);
 		}
 		if ((poly1 == 'A' || poly1 == 'C') && (poly2 == 'C' || poly2 == 'A')) {
