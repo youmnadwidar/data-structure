0a5c040b1e76db21c9cbb59fa8167270f066f535
youmna_dwidar <youmnadwidar@hotmail.com>
2017-04-24 00:01:33 +0200

    1

--- scr/src/eg/edu/alexu/csd/datastructure/stack/cs74/Stack.java
+++ scr/src/eg/edu/alexu/csd/datastructure/stack/cs74/Stack.java
@@ -2,15 +2,18 @@ package eg.edu.alexu.csd.datastructure.stack.cs74;
 
 import eg.edu.alexu.csd.datastructure.linkedList.cs74.DoublyLinkedList;
 import eg.edu.alexu.csd.datastructure.stack.IStack;
-
+/**
+ * @author HP.
+ *
+ */
 public class Stack implements IStack {
-
-  DoublyLinkedList stack = new DoublyLinkedList();
-
+/**
+ * object doublyLinkedList.
+ */
+ private DoublyLinkedList stack = new DoublyLinkedList();
   @Override
   /**
    * Inserts a specified element at the specified position in the list.
-   * 
    * @param index
    *          zero-based index
    * @param element
@@ -25,7 +28,7 @@ public class Stack implements IStack {
   @Override
   /**
    * Removes the element at the top of stack and returns that element.
-   * 
+
    * @return top of stack element, or through exception if empty
    */
   public Object pop() {
@@ -38,7 +41,6 @@ public class Stack implements IStack {
 
   /**
    * Get the element at the top of stack without removing it from stack.
-   * 
    * @return top of stack element, or through exception if empty
    */
   public Object peek() {
@@ -50,7 +52,6 @@ public class Stack implements IStack {
   @Override
   /**
    * Pushes an item onto the top of this stack.
-   * 
    * @param object
    *          to insert
    */
@@ -61,7 +62,6 @@ public class Stack implements IStack {
   @Override
   /**
    * Tests if this stack is empty
-   * 
    * @return true if stack empty
    */
   public boolean isEmpty() {
@@ -72,7 +72,7 @@ public class Stack implements IStack {
   @Override
   /**
    * Returns the number of elements in the stack.
-   * 
+
    * @return number of elements in the stack
    */
   public int size() {
@@ -81,4 +81,6 @@ public class Stack implements IStack {
   }
 
   
+
+
 }
