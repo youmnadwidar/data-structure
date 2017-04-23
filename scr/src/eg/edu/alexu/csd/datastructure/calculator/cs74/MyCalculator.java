3b7c16c86bf45fb0f257e88bffc82f0b4f9a9a82
youmna_dwidar <youmnadwidar@hotmail.com>
2017-04-24 00:29:46 +0200

    0

--- scr/src/eg/edu/alexu/csd/datastructure/calculator/cs74/MyCalculator.java
+++ scr/src/eg/edu/alexu/csd/datastructure/calculator/cs74/MyCalculator.java
@@ -1,7 +1,11 @@
 package eg.edu.alexu.csd.datastructure.calculator.cs74;
 
 import eg.edu.alexu.csd.datastructure.calculator.ICalculator;
-
+/**
+ * .
+ * @author HP
+ *
+ */
 public class MyCalculator implements ICalculator {
   /**
    * Adds given two numbers.
@@ -12,7 +16,7 @@ public class MyCalculator implements ICalculator {
    *          second number
    * @return the sum of the two numbers
    */
-  public final int add(final int x,final  int y) {
+  public final int add(final int x, final  int y) {
 
     return x + y;
   }
@@ -24,7 +28,7 @@ public class MyCalculator implements ICalculator {
    *          second number
    * @return the division result
    */
-  public float divide(int x, int y) {
+  public float divide(final int x, final int y) {
     return (float) x / (float) y;
   }
 
