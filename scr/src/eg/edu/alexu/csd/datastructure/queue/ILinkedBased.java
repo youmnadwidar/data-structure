cf1f90853b3b8554b8c390f530287e1a282873ad
youmna_dwidar <youmnadwidar@hotmail.com>
2017-05-03 01:24:23 +0200

    0

--- scr/src/eg/edu/alexu/csd/datastructure/queue/ILinkedBased.java
+++ scr/src/eg/edu/alexu/csd/datastructure/queue/ILinkedBased.java
@@ -1,5 +1,4 @@
 package eg.edu.alexu.csd.datastructure.queue;
 
 public interface ILinkedBased extends IQueue {
-
 }
--- scr/src/eg/edu/alexu/csd/datastructure/queue/IQueue.java
+++ scr/src/eg/edu/alexu/csd/datastructure/queue/IQueue.java
@@ -3,24 +3,29 @@ package eg.edu.alexu.csd.datastructure.queue;
 public interface IQueue {
   /**
    * Inserts an item at the queue front.
-   * @param item that wanted to be added
+   * 
+   * @param item
+   *          that wanted to be added
    */
   public void enqueue(Object item);
 
   /**
    * Removes the object at the queue rear and returns it.
+   * 
    * @return the dequeued item
    */
   public Object dequeue();
 
   /**
    * Tests if this queue is empty.
+   * 
    * @return whether it is empty or not
    */
   public boolean isEmpty();
 
-  /**.
-   * Returns the number of elements in the queue.
+  /**
+   * . Returns the number of elements in the queue.
+   * 
    * @return size
    */
   public int size();
