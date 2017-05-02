8c2967b8fc9ea401f2092bdcb2ee0994c32b3a93
youmna_dwidar <youmnadwidar@hotmail.com>
2017-05-03 01:17:58 +0200

    0

--- scr/src/eg/edu/alexu/csd/datastructure/queue/cs74/LinkedBasedQueue.java
+++ scr/src/eg/edu/alexu/csd/datastructure/queue/cs74/LinkedBasedQueue.java
@@ -8,24 +8,22 @@ public class LinkedBasedQueue implements ILinkedBased{
   
 DoublyLinkedList queue=new DoublyLinkedList();  
   
-  @Override
+  
   public void enqueue(Object item) {
 queue.add(0, item);    
   }
 
-  @Override
   public Object dequeue() {
    Object item= queue.get(queue.getSize()-1);
    queue.remove(queue.getSize()-1);
     return item;
   }
 
-  @Override
   public boolean isEmpty() {
     return queue.isEmpty();
   }
 
-  @Override
+  
   public int size() {
     return queue.size();
   }
