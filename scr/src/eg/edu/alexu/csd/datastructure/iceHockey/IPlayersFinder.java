1bfc5e7cd48915101cc62d17065025d874ba468a
youmna_dwidar <youmnadwidar@hotmail.com>
2017-05-04 00:38:06 +0200

    0

--- scr/src/eg/edu/alexu/csd/datastructure/iceHockey/IPlayersFinder.java
+++ scr/src/eg/edu/alexu/csd/datastructure/iceHockey/IPlayersFinder.java
@@ -1,13 +1,16 @@
 package eg.edu.alexu.csd.datastructure.iceHockey;
 
 import java.awt.Point;
-
+/**
+ * 
+ * @author HP.
+ *
+ */
 public interface IPlayersFinder {
   /**
-   * Search for players locations at the given photo
-   * 
-   * @param photo
-   *          Two dimension array of photo contents Will contain between 1 and 50 elements,
+   * Search for players locations at the given photo.
+   * @param photo Two dimension array of photo contents
+   *  Will contain between 1 and 50 elements,
    *          inclusive
    * @param team
    *          Identifier of the team
@@ -18,15 +21,15 @@ public interface IPlayersFinder {
   java.awt.Point[] findPlayers(String[] photo, int team, int threshold);
 
   /**
-   * find the players by DFS
-   * 
+   * find the players by DFS.
    * @param row
    *          the row the index of the cell
    * @param col
    *          the column index of the cell
-   * @param photo
-   *          Two dimension array of photo contents Will contain between 1 and 50 elements,
-   *          inclusive * @param team
+   * @param photo Two dimension array of photo contents
+   *           Will contain between 1 and 50 elements,
+   *          inclusive
+   * @param team the number of the wanted team
    * @return the center point of the player
    */
   Point players(int row, int col, String[] photo, int team);
