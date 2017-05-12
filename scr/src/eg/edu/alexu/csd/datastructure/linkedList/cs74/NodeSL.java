5bb29c91610421da39f2b21c13cc2668202e5fb0
youmna_dwidar <youmnadwidar@hotmail.com>
2017-05-12 21:27:44 +0200

    0

--- scr/src/eg/edu/alexu/csd/datastructure/linkedList/cs74/NodeSL.java
+++ scr/src/eg/edu/alexu/csd/datastructure/linkedList/cs74/NodeSL.java
@@ -1,40 +1,62 @@
 package eg.edu.alexu.csd.datastructure.linkedList.cs74;
-
+/**
+ * .
+ * @author HP
+ *
+ */
 public class NodeSL {
 /**
  * data of the node.
  */
   private Object data;
   /**
-   * 
+
    */
 
   private NodeSL next;
-
-  public NodeSL(Object value) {
+/**
+ * .
+ * @param value the value of the elemnt.
+ */
+  public NodeSL(final Object value) {
     this.data = value;
     this.next = null;
   }
-
+/**
+ * @param value .
+ * @param after .
+ */
   public NodeSL(final Object value, final NodeSL after) {
 
     this.data = value;
     this.next = after;
   }
-
+/**
+ *
+ * @return .
+ */
   public Object getData() {
     return data;
   }
-
-  public void setData(Object value) {
+/**
+ * setter.
+ * @param value .
+ */
+  public void setData(final Object value) {
     this.data = value;
   }
-
+/**
+ *
+ * @return next.
+ */
   public NodeSL getNext() {
     return next;
   }
-
-  public void setNext(NodeSL after) {
+/**
+ *.
+ * @param after .
+ */
+  public void setNext(final NodeSL after) {
     this.next = after;
   }
 
