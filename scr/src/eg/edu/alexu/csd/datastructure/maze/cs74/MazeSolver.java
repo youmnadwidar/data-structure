f636ee50b30d6a6055b1f6ee0fc7cf1a7a841eea
youmna_dwidar <youmnadwidar@hotmail.com>
2017-05-13 22:50:32 +0200

    0

--- scr/src/eg/edu/alexu/csd/datastructure/maze/cs74/MazeSolver.java
+++ scr/src/eg/edu/alexu/csd/datastructure/maze/cs74/MazeSolver.java
@@ -138,13 +138,13 @@ public class MazeSolver implements IMazeSolver {
         operate.push(new Point(temp.x, temp.y - 1));
         visited[temp.x][temp.y - 1] = true;
       }
-      if(temp.y + 1 >=mazeArray[0].length){
+      if(temp.y + 1 <mazeArray[0].length){
       if (checkValid(new Point(temp.x, temp.y + 1), visited)) {
         operate.push(new Point(temp.x, temp.y + 1));
         visited[temp.x][temp.y + 1] = true;
       }
       }else {
-        throw new RuntimeException(mazeArray.toString());
+        throw new RuntimeException(mazeArray[0].toString());
       }
       if (checkValid(new Point(temp.x - 1, temp.y), visited)) {
         operate.push(new Point(temp.x - 1, temp.y));
